a3eb4d958f839b785f5e5253cdf366157efca509
Jason Boerner <jasonboerner@Jasons-MBP.T-mobile.com>
2019-02-28 13:43:59 -0500

    adds css transitions

--- simple-ig.js
+++ simple-ig.js
@@ -2,16 +2,19 @@
 // const is a "constant" variable that will not be assigned.
 const targetElements = document.querySelectorAll("[data-target]");
 
-function respondToClick(){
+function respondToClick(event){
     console.log("hello");
     console.log("I am Doggo")
     console.log("Give me food")
+    
+    console.log(event.target);
+    // add the big class to the thing that was clicked.
+    // toggle allows the effect to be turned on and off.
+    event.target.classList.toggle('big')
 }
 
 function attachClickHandler(oneElement){
     // this is how one element is handled
     oneElement.addEventListener("click", respondToClick);
 }
-
-
 targetElements.forEach(attachClickHandler);
